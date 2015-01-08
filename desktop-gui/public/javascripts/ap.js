8103d13d7903adf4004b5816e69b952692bea930
Oytun <oytunozdemir@gmail.com>
2015-01-08 15:06:28 +0200

    / ‘desktop-gui/public/javascripts/ap.js’

--- desktop-gui/public/javascripts/ap.js
+++ desktop-gui/public/javascripts/ap.js
@@ -1,3 +1,10 @@
 var ap = function(){
   this.ver = "0.1";
+  this.module = false;
+  this.debug = false;
+  if(this.module){
+    if(this.debug){
+      console.log(this.module + ' is loaded.');
+    }
+  }
 };
