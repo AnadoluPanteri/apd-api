9645a62ba0dea035dfc68c45c0c3b598b2abc2e7
Oytun <oytunozdemir@gmail.com>
2015-01-08 17:28:56 +0200

    / ‘desktop-gui/routes/filemanager.js’

--- desktop-gui/routes/filemanager.js
+++ desktop-gui/routes/filemanager.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/folder',function(req, res){
+  res.type('application/json');
   res.format({
     'text/plain': function(){
       res.send('hey');
