94cd698897887f92f0eff2e15d65c7ab6556d3a7
Oytun <oytunozdemir@gmail.com>
2015-01-08 17:52:38 +0200

    / ‘desktop-gui/app.js’

--- desktop-gui/app.js
+++ desktop-gui/app.js
@@ -23,8 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/fs', filemanager);
-app.get('/fs/:id', get.one);
+app.use('/fs', filemanager.main);
+app.get('/fs/:id', filemanager.one);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
