09346eddb3b23e4028c86f6dbc96acc361951510
Oytun <oytunozdemir@gmail.com>
2015-01-08 17:53:39 +0200

    / ‘desktop-gui/routes/filemanager.js’

--- desktop-gui/routes/filemanager.js
+++ desktop-gui/routes/filemanager.js
@@ -1,16 +1,1 @@
-var express = require('express');
-var router = express.Router();
-//var fs = require('file');
-
-/* GET home page. */
-
-/*
-get all files to render home
-*/
-router.get('/', function(req, res) {
-  res.render('filemanager', {
-     title: 'Anadolu Panteri Desktop | Dosya Yöneticisi'
-  });
-});
-
-module.exports = router;
+var fs = require('fs');
