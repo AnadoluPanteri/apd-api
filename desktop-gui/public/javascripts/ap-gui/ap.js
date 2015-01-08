af80ed5f98a0cf3c5cf3e2f956ce2e7d45ecda40
Oytun <oytunozdemir@gmail.com>
2015-01-08 15:58:34 +0200

    / ‘desktop-gui/public/javascripts/ap-gui/ap.js’

--- desktop-gui/public/javascripts/ap-gui/ap.js
+++ desktop-gui/public/javascripts/ap-gui/ap.js
@@ -31,7 +31,11 @@ var ApCore = function(){
     t.parentNode.insertBefore(s, t);
   }
 
-  this.jquery = function(function){
-    this.js('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',function);
+  this.jquery = function(func){
+    this.js('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',function(){
+      if(func instanceof Function){
+        func();
+      }
+    });
   }
 };
