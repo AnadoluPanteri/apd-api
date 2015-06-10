7c4d6773f01bdd9081eba91fd41e506fa3563b5a
Oytunistrator <oytunozdemir@gmail.com>
2015-06-10 09:18:14 +0300

    test

--- desktop-gui/routes/index.js
+++ desktop-gui/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Anadolu Panteri Desktop' });
+  res.render('index', {
+    title: 'Anadolu Panteri Desktop',
+  });
 });
 
 module.exports = router;
